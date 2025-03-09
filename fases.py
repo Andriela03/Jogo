de02f6dbc29a1535815affad65346f383d06d1b0
Andriela Gleice <andriela.silva@escolar.ifrn.edu.br>
2025-03-09 15:02:00 -0300

    Acrescentando mais escolha do jogo

--- fases.py
+++ fases.py
@@ -36,16 +36,16 @@ class PontoInicial(Fase):
         if escolha == 0:
             return Preguica_1()
         else:
-            return Parte5()
+            return Vila_1()
         
 class Sair_do_Jogo(Fase): #Parte que sai do jogo
     
     def executar(self):   
         return None
         
-class Preguica_1(Fase):
+class Preguica_1(Fase): #Primeira parte da preguiça
     def __init__(self):
-        self.__descricao ='''\033Preguiça\033[0m \n Seguindo pelo Vale dos Lírios, Nilo avista, à frente, o primeiro reino: o Pecado da Preguiça. Um arrepio percorre sua espinha, mas ele se mantém firme e continua a caminhada. 
+        self.__descricao ='''Seguindo pelo Vale dos Lírios, Nilo avista, à frente, o primeiro reino: o Pecado da Preguiça. Um arrepio percorre sua espinha, mas ele se mantém firme e continua a caminhada. 
         Para chegar lá, é preciso atravessar uma pequena floresta. O vento sopra suavemente entre as árvores, e o silêncio quase absoluto faz com que cada passo pareça mais lento, dando uma sensação de cansaço. 
         De repente, uma linda voz ecoa por um dos caminhos da floresta, cantando uma doce melodia de ninar. Curioso, Nilo...
         '''
@@ -59,30 +59,82 @@ class Preguica_1(Fase):
         escolha = JogoUtil.fazer_escolha(self.__opcoes)
         
         if escolha == 0:
-            return Parte6()
+            return Preguica_2()
         else:
             return Parte7()
 
-class Parte5(Fase):
+class Vila_1(Fase): #Primeira parte da vila
+    def __init__(self):
+        self.__descricao ='''Nilo chega à entrada da pequena vila, escurecida pelas sombras da noite fria. Um morador o recebe, Rick, e o leva para sua cabana. Sorridente, ele conta um pouco sobre sua vida e a vila; todos são simpáticos e prestativos. Nilo faz uma refeição e é acomodado em um quarto, adormecendo. Na manhã seguinte...
+        '''
+        self.__opcoes = ["Ir conhecer a vila", "Ficar na moradia pela manhã"]
     
     def executar(self):
-        print('''Você segue para a ala de cirurgia. Lá 
-              você encontra o remédio na estante.
-              ''')
+        print("\nParte 4")
+        print(self.__descricao)
+        JogoUtil.exibir_opcoes(self.__opcoes)
+        escolha = JogoUtil.fazer_escolha(self.__opcoes)
         
-        return None
+        if escolha == 0:
+            return Parte8()
+        else:
+            return Parte9()
 
-class Parte6(Fase):
+class Preguica_2(Fase): #Primeira escolha da preguiça_1
+    
+    def __init__(self):
+        self.__descricao ='''A bela voz continua a ecoar suavemente, conduzindo Nilo até uma pequena cabana aconchegante, escondida entre as árvores. O interior do local traz uma sensação de acolhimento, com uma lareira crepitando suavemente em um canto, iluminando o ambiente com uma luz suave e quente. No centro, dois objetos repousam sobre uma mesa de madeira escura, cada um irradiando um brilho peculiar. Nilo se aproxima e observa com atenção. Cada item parece ser útil, mas algum não irá ajudá-lo... (Seu inventário pode ajudar?)
+        '''
+        self.__opcoes = ["Ir conhecer a vila", "Ficar na moradia pela manhã", "Verificar o inventário"]
     
     def executar(self):
-        print('''Você se depara com diversos experimentos
-              com pessoas. lá você cai em uma armadilha
-              e morre!
-              ''')
+        print("\n")
+        print(self.__descricao)
+        JogoUtil.exibir_opcoes(self.__opcoes)
+        escolha = JogoUtil.fazer_escolha(self.__opcoes)
         
-        return None
+        if escolha == 0:
+            return Preguica_4()
+        
+        elif escolha == 1:
+            return Parte9()
+        
+        elif escolha == 2:
+            # Verifica se o inventário está vazio
+            if Inventario.esta_vazio():
+                print("\n\033[91m\nO inventário está vazio.\033[0m")
+            else:
+                # Mostra o inventário
+                Inventario.mostrar_inventario()
+                
+                # Verifica se a "lista" está no inventário
+                if Inventario.verificar_item("lista"):
+                    print('\nNilo abre a lista e lê a seguinte charada: \n\n"Sou um convite ao descanso, mas também ao atraso. Quanto mais tempo comigo, menos vontade de me deixar. Quem sou eu?"')
+                else:
+                    print('\nVocê não possui a lista.')
+                
+            return Preguica_2()
+        
+
+class Preguica_4(Fase): 
     
-class Parte7(Fase):
+    def __init__(self):
+        self.__descricao ='''Nilo chega à entrada da pequena vila, escurecida pelas sombras da noite fria. Um morador o recebe, Rick, e o leva para sua cabana. Sorridente, ele conta um pouco sobre sua vida e a vila; todos são simpáticos e prestativos. Nilo faz uma refeição e é acomodado em um quarto, adormecendo. Na manhã seguinte...
+        '''
+        self.__opcoes = ["Ir conhecer a vila", "Ficar na moradia pela manhã"]
+    
+    def executar(self):
+        print("\nParte 4")
+        print(self.__descricao)
+        JogoUtil.exibir_opcoes(self.__opcoes)
+        escolha = JogoUtil.fazer_escolha(self.__opcoes)
+        
+        if escolha == 0:
+            return Parte8()
+        else:
+            return Parte9()
+    
+class Parte9(Fase): #Segunda esclha da preguiça_1
     
     def executar(self):
         print('''Você encontra o remédio na gaveta da mesa.
--- inventario.py
+++ inventario.py
@@ -1,25 +1,30 @@
-class Inventario: 
-    def __init__(self, itens):
-        self.itens = []
-        
-        
-    def adicionar_item(self):
-        self.itens.append(item)
-        print(f"{item} foi adicinado no seu inventário.")
-        
-    def verificar_item (self, item):
-        if item in self.itens:
-            print(f"{item} está no inventário")
-            
-        else: 
-            print( f"{item} não está no seu inventário.")
-            
-    def mostrar_inventário(self):
-        if self.itens:
-            print("Inventário")
-            for item in self.itens:
+class Inventario:
+    itens = []  # Atributo de classe para armazenar os itens
+
+    @classmethod
+    def adicionar_item(cls, item):
+        cls.itens.append(item)
+        print(f"{item} foi adicionado ao seu inventário.")
+
+    @classmethod
+    def verificar_item(cls, item):
+        if item in cls.itens:
+            print(f"{item} está no inventário.")
+            return True
+        else:
+            print(f"{item} não está no seu inventário.")
+            return False
+
+    @classmethod
+    def mostrar_inventario(cls):
+        if cls.itens:
+            print("Inventário:")
+            for item in cls.itens:
                 print(f"- {item}")
         else:
             print("O inventário está vazio.")
             
             
+    @classmethod
+    def esta_vazio(cls):
+        return len(cls.itens) == 0
